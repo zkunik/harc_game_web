2a56cab14099fea97f678488ea6a9db3d7253964
tqZK <zuzannakunik@gmail.com>
2020-11-07 17:12:24 +0100

    Add login pages etc.

--- harc_game_web/harc_game_web/urls.py
+++ harc_game_web/harc_game_web/urls.py
@@ -4,9 +4,12 @@ harc_game_web URL Configuration
 from django.contrib import admin
 from django.urls import path
 from apps.core.views import frontpage, signup
+from django.contrib.auth.views import LogoutView, LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', frontpage, name='frontpage'),
-    path('signup/', signup, name='signup')
+    path('signup/', signup, name='signup'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('login/', LoginView.as_view(template_name='core/login.html'), name='login')
 ]
