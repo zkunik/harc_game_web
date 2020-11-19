4c843a1d2081ecc4f9e4649efdada46e825913da
Wojtek Doganowski <wojciech@doganowscy.com>
2020-11-19 21:07:21 +0100

    Dodane zastepy i harcerze do widoku druzyny

--- harc_game_web/apps/teams/views.py
+++ harc_game_web/apps/teams/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views import View
 
-from apps.teams.models import Team
+from apps.teams.models import Team, Patrol
+from apps.users.models import Scout
+from apps.tasks.models import Task, DocumentedTask, TaskApproval
+
 
 class TeamView(View):
 
@@ -17,5 +20,28 @@ class TeamView(View):
         Function to view
         """
         team = Team.objects.get(id=id)
-        return render(request, 'teams/view.html', {'team': team})
+        patrols = Patrol.objects.filter(team=team)
+        scouts = Scout.objects.filter(team=team)
+        team_leader = scouts.filter(is_team_leader=True).first()
+
+# def score(self):
+    #     """
+    #     Obliczenie wyniku
+    #     """
+    #     from apps.tasks.models import Task, DocumentedTask, TaskApproval
+
+    #     print(DocumentedTask.objects.all())
+    #     print(DocumentedTask.objects.select_related('task')) 
+
+    #     #return Task.objects.filter(user=self.user).select_related('DocumentedTask').aggregate(Sum('prize'))
+
+    #     print(
+    #         #DocumentedTask.objects.filter(user=self.user).filter(taskapproval__is_accepted=True)
+    #         TaskApproval.objects.all()#filter(is_accepted=True).all()
+    #     )
+    #     return Task.objects.aggregate(Sum('prize'))
+    
+        print(TaskApproval.objects.all())
+
+        return render(request, 'teams/view.html', {'team': team, 'leader': team_leader, 'patrols': patrols, 'scouts': scouts})
 
--- harc_game_web/apps/users/admin.py
+++ harc_game_web/apps/users/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from apps.users.views import HarcgameUserCreationForm
-from apps.users.models import HarcgameUser
+from apps.users.models import HarcgameUser, Scout
 
 
 class CustomUserAdmin(UserAdmin):
@@ -25,3 +25,4 @@ class CustomUserAdmin(UserAdmin):
 
 
 admin.site.register(HarcgameUser, CustomUserAdmin)
+admin.site.register(Scout)
