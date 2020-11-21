c32b6a58cf115bebd1f984010dd8480da777424c
Zuzanna Kunik <zuzannakunik@gmail.com>
2020-11-21 12:23:26 +0100

    Fix frontend styling.

--- harc_game_web/harc_game_web/settings.py
+++ harc_game_web/harc_game_web/settings.py
@@ -82,6 +82,10 @@ TEMPLATES = [
     },
 ]
 
+STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
 WSGI_APPLICATION = 'harc_game_web.wsgi.application'
 
 
