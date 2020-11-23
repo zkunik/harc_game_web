68648f089960439178dd6fcc926cdf3c87c55767
wdoganowski <wojciech@doganowscy.com>
2020-11-23 22:33:13 +0100

    Small fixes (#71)
    * Ponowna poprawka na sytuacje, że są dwa hasła na dzień
    
    * Fix na relacje documentedtasks
    
    * Added task description to list of tasks
    
    * Fixed adding word of the day via admin

--- harc_game_web/apps/wotd/models.py
+++ harc_game_web/apps/wotd/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 class WordOfTheDay(models.Model):
     """
-    Odpowiedzi w formie daty lub słowa na pytanie danego dnia 
+    Odpowiedzi w formie daty lub słowa na pytanie danego dnia
 
     Hasło dnia ma być podawane podczas wykonywania zadania danego dnia w celu uniknięcia oszustw
 
@@ -15,18 +15,12 @@ class WordOfTheDay(models.Model):
         Hasło dnia (rozwiązanie)
         Dzień, do którego przypisane jest hasło
         """
-        
+
     #id jest tworzone automatycznie
     question = models.TextField(help_text='Można używać tagów HTML')
     hint = models.CharField(max_length=400)
     answer = models.CharField(max_length=400)
     date = models.DateField('Kiedy ma być opublikowany', default=timezone.now)
-
-    def save(self, *args, **kwargs):
-        if self._state.adding and not self.slug:
-            # Newly created object, so set timestamp
-            self.date_time = timezone.now()
-        return super(WordOfTheDay, self).save(*args, **kwargs)
 
     def __str__(self):
         return f'Pytanie na {self.date}'
