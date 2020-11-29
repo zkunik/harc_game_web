38fa8df3d89f56255b541df4eb3bb790258ef620
Zuzanna Kunik <zuzannakunik@gmail.com>
2020-11-29 20:15:49 +0100

    Added editing documented tasks. Add volume in docker-compose (#81)
    * Add volume to docker compose. Set media root to docker volume path.
    
    * Changes in Makefile
    
    * Added editing tasks
    
    * Update github test check

--- harc_game_web/harc_game_web/settings.py
+++ harc_game_web/harc_game_web/settings.py
@@ -91,9 +91,6 @@ MESSAGE_TAGS = {
     messages.ERROR: 'rpgui-list-imp'
 }
 
-STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = [BASE_DIR / 'static']
 
 WSGI_APPLICATION = 'harc_game_web.wsgi.application'
 
@@ -146,4 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = "media/"
+# STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_ROOT = "/var/www/media/"
