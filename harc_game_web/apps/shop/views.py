30eb44df11a863947fda84880a000489c4311824
wdoganowski <wojciech@doganowscy.com>
2020-12-02 20:20:42 +0100

    Add shop app (#95)
    * DB definition
    
    * + db migrations

---
+++ harc_game_web/apps/shop/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+"""
+Harcerze powinni widzieć, co będą mogli kupić w grze za punkty zdobyte za zadania w aplikacji. Należy stworzyć podstronę (app) zawierającą aktualną ofertę.
+Harcerz może również zgłosić prośbę o jakiś przedmiot poprzez formularz próśb.
+
+Podstrona oferty sklepu (mogą być 2 widoki albo 1 - do przemyślenia/sprawdzenia)
+
+subnavbar
+widoki:
+-- tabela z aktualnymi ofertami
+-- prosty formularz próśb z wyświetlaniem próśb poniżej
+"""
