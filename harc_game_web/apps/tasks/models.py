154c769a96d55b4bf0e5857a7fdc289fd08b7a35
Czapla <56671347+Antoni-Czaplicki@users.noreply.github.com>
2020-12-04 08:34:21 +0100

    Update models.py

--- harc_game_web/apps/tasks/models.py
+++ harc_game_web/apps/tasks/models.py
@@ -70,7 +70,7 @@ class DocumentedTask(models.Model):
     Model udokumentowanego wykonanego zadania
     """
     task = models.ForeignKey(Task, on_delete=models.RESTRICT, null=True, default=None)
-    how_many_times =  models.PositiveSmallIntegerField("Ile razy zadanie zostało wykonane", null = True, default=1)
+    how_many_times =  models.PositiveSmallIntegerField("Ile razy zadanie zostało wykonane", null = True, default=1, validators=[MaxValueValidator(100), MinValueValidator(1)])
     user = models.ForeignKey(HarcgameUser, on_delete=models.RESTRICT, null=True, default=None)
     date_completed = models.DateTimeField("Data ukończenia", default=timezone.now)
     date_last_edited = models.DateTimeField("Data ostatniej edycji", null=True, default=None)
